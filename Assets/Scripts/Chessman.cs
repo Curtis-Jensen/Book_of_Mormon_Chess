75b19a5dc59f2479c72a29b07fd88ec5bf2cc7f4
Curtis-Jensen <me@curtisjensen.com>
2024-06-30 09:45:27 -0600

    Initial push

---
+++ Assets/Scripts/Chessman.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chessman : MonoBehaviour
+{
+    public GameObject controller;
+    public GameObject movePlate;
+
+    void Start()
+    {
+        
+    }
+}

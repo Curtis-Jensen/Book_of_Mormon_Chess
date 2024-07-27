28346d7c68001501e8e33923c1e5616c7fbd0639
Curtis Jensen <me@curtisjensen.com>
2024-07-27 14:42:43 -0600

    not much

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    
+    void OnMouseDown()
+    {
+        // Check if the tile has a piece
+        if (transform.childCount <= 0) return;
+
+        // Get the child object (assuming there is only one child)
+        Transform child = transform.GetChild(0);
+        Piece piece = child.GetComponent<Piece>();
+
+        if (piece != null)
+        {
+            // Access the move data or any other data from the piece script
+            Debug.Log("Tile clicked. Piece is white: " + piece.isWhite);
+        }
+    }
 }
+

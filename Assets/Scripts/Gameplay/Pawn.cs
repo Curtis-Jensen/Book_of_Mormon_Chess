3fbb238915b4ec61efb24a126e7e416e88935aec
Curtis Jensen <me@curtisjensen.com>
2024-08-17 16:13:51 -0600

    small change

--- Assets/Scripts/Gameplay/Pawn.cs
+++ Assets/Scripts/Gameplay/Pawn.cs
@@ -6,14 +6,14 @@ public class Pawn : Piece
 {
     public override List<Vector2Int> GetMoves()
     {
-        List<Vector2Int> validMoves = new List<Vector2Int>();
+        List<Vector2Int> validMoves = new();
 
         //If it's white, go up, if it's black, go down
         int forward = isWhite ? 1 : -1;
 
         // Check one square forward
         Vector2Int forwardMove = 
-            new Vector2Int((int)transform.position.x, (int)transform.position.y + forward);
+            new ((int)transform.position.x, (int)transform.position.y + forward);
         if (Board.Instance.IsTileEmpty(forwardMove))
         {
             validMoves.Add(forwardMove);
@@ -22,8 +22,8 @@ public class Pawn : Piece
         // Check diagonal capture moves
         Vector2Int[] diagonalMoves = new Vector2Int[]
         {
-            new Vector2Int((int)transform.position.x - 1, (int)transform.position.y + forward),
-            new Vector2Int((int)transform.position.x + 1, (int)transform.position.y + forward)
+            new ((int)transform.position.x - 1, (int)transform.position.y + forward),
+            new ((int)transform.position.x + 1, (int)transform.position.y + forward)
         };
 
         foreach (var move in diagonalMoves)

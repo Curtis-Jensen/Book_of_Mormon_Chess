1e3628ff26342292222aa5d3fa90728c9d2c434d
Curtis Jensen <me@curtisjensen.com>
2024-08-25 07:13:04 -0600

    Added gameplay for the king!!

--- Assets/Scripts/Gameplay/Piece Scripts/King.cs
+++ Assets/Scripts/Gameplay/Piece Scripts/King.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
-public class King : MonoBehaviour
+public class King : Piece
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //The four cardinal directions, and diagonal moves, to be multiplied upon
+    Vector2Int[] moveDirections =
+        { new( 1,  0),
+          new(-1,  0),
+          new(0,   1),
+          new(0,  -1),
+          new( 1,  1),
+          new(-1, -1),
+          new(-1,  1),
+          new( 1, -1)};
 
-    // Update is called once per frame
-    void Update()
+    public override List<Vector2Int> GetMoves()
     {
-        
+        List<Vector2Int> validMoves = new();
+
+        foreach (var moveDirection in moveDirections)
+        {
+            Vector2Int newMove =
+                new((int)transform.position.x + moveDirection.x, (int)transform.position.y + moveDirection.y);
+
+            if (Board.Instance.IsTileEmpty(newMove))
+            {
+                validMoves.Add(newMove);
+                continue;
+            }
+            else if (Board.Instance.IsEnemyPiece(newMove, isWhite))
+            {
+                validMoves.Add(newMove);
+            }
+        }
+
+        return validMoves;
     }
 }

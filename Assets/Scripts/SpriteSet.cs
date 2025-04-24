56fea78233a6de6edbf987a88c46eb0e7f6eea88
Curtis Jensen <me@curtisjensen.com>
2025-04-23 19:44:32 -0600

    pixel art technically works

--- Assets/Scripts/SpriteSet.cs
+++ Assets/Scripts/SpriteSet.cs
@@ -5,6 +5,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SpriteSet", menuName = "Custom/SpriteSet")]
 public class SpriteSet : ScriptableObject
 {
-    public string setName; [Tooltip("e.g., 'Classic', 'Modern'")]
     public Sprite King, Queen, Rook, Bishop, Knight, Pawn;
 }

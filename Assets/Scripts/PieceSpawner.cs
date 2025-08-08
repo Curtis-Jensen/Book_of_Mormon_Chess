15469c09c430d99f9a56336e8193dc98050c938b
Curtis Jensen <38483758+Curtis-Jensen@users.noreply.github.com>
2025-08-08 12:27:40 -0600

    Created piece spawner

--- Assets/Scripts/PieceSpawner.cs
+++ Assets/Scripts/PieceSpawner.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class PieceSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public PieceSets pieceSets;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }

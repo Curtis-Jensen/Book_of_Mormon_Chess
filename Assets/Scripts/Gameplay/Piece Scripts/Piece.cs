08cb3515eb5b7ae40edc6fe88526fb2ddc4e17f3
Curtis-Jensen <me@curtisjensen.com>
2025-08-22 19:48:00 -0600

    moved destroying into the piece script

--- Assets/Scripts/Gameplay/Piece Scripts/Piece.cs
+++ Assets/Scripts/Gameplay/Piece Scripts/Piece.cs
@@ -11,10 +11,42 @@ public abstract class Piece : MonoBehaviour
     public bool firstTurnTaken = false;
     [HideInInspector]
     public int playerIndex;
+    public GameObject destroyParticlesPrefab;
+    public GameObject ghost;
 
     /// <summary>
     /// Abstract method for movement logic
     /// </summary>
     /// <returns></returns>
     public abstract List<Vector2Int> GetMoves();
+
+    public void Die()
+    {
+        //Set the position the particles need to spawn to be visible
+        Vector3 position = 
+            new Vector3(transform.position.x, transform.position.y, -5);
+
+        InstantiateDeathEffects();
+
+        FindAnyObjectByType<AiManager>().aiPieces.Remove(this);
+        Destroy(gameObject);
+    }
+
+    void InstantiateDeathEffects()
+    {
+        // Spawn particles
+        Instantiate(destroyParticlesPrefab, transform.position, Quaternion.identity);
+
+        //Spawn ghost
+        Quaternion randomRotation = Quaternion.Euler(0f, 0f, Random.Range(0f, 360f));
+        var ghostInstance = Instantiate(ghost, transform.position, randomRotation);
+
+        ghostInstance.GetComponent<SpriteRenderer>().sprite = 
+        GetComponent<SpriteRenderer>().sprite;
+
+        var capturedPieceColor = GetComponent<SpriteRenderer>().color;
+
+        ghostInstance.GetComponent<SpriteRenderer>().color = new Color(capturedPieceColor.r,
+            capturedPieceColor.g, capturedPieceColor.b, 0.25f);
+    }
 }

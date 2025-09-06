3a9bf04df60b78087cc4fad0db08c87adc5796a9
Curtis Jensen <38483758+Curtis-Jensen@users.noreply.github.com>
2025-09-06 15:26:03 -0600

    Made player it’s own file

--- Assets/Scripts/SceneStart/BoardSetup.cs
+++ Assets/Scripts/SceneStart/BoardSetup.cs
@@ -4,14 +4,6 @@ using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UIElements;
 
-[System.Serializable]
-public class Player
-{
-    public string name;
-    public bool isAi;
-    public bool teamOne;
-}
-
 [RequireComponent(typeof(AiManager))]
 [RequireComponent(typeof(TurnManager))]
 public class BoardSetup : MonoBehaviour

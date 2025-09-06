bd6376a4eaa20a21ce1d28cf8c3d7a8e75174366
Curtis Jensen <me@curtisjensen.com>
2025-09-06 14:44:55 -0600

    Refactored the code to use SceneConfig

--- Assets/Scripts/SceneStart/SceneLoader.cs
+++ Assets/Scripts/SceneStart/SceneLoader.cs
@@ -1,21 +1,31 @@
-﻿using TMPro;
+﻿using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
+[Serializable]
+public class SceneConfig
+{
+    public string dropDownOptionName;
+    public string sceneName;
+    public GameObject[] backRowPrefabs;
+}
 
 public class SceneLoader : MonoBehaviour
 {
     public TextMeshProUGUI sizeInput;
     public TMP_Dropdown modeDropdown;
-    public string sceneName;
+    public SceneConfig[] sceneConfigs;
 
+    //Called by the main menu so it knows which scene to go to
     public void SetupNewScene()
     {
         PlayerPrefs.SetInt("boardSize", GetInputText(sizeInput));
 
-        sceneName = modeDropdown.options[modeDropdown.value].text;
+        var sceneName = modeDropdown.options[modeDropdown.value].text;
 
-        LoadScene();
+        LoadScene(sceneName);
     }
 
     #region Private Methods
@@ -27,7 +37,8 @@ public class SceneLoader : MonoBehaviour
         return int.Parse(cleanedText);
     }
 
-    public void LoadScene()
+    //Called by the main menu button to be hardcoded to one scene.  Also called by SetupNewScene to load the selected scene
+    public void LoadScene(string sceneName = "Main Menu")
     {
         SceneManager.LoadScene(sceneName);
     }

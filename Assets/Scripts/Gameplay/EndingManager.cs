21d20059b97f74cd1220b85d774d6a65719ac2d2
Curtis Jensen <38483758+Curtis-Jensen@users.noreply.github.com>
2025-09-07 19:19:01 -0600

    tried putting some more logging in

--- Assets/Scripts/Gameplay/EndingManager.cs
+++ Assets/Scripts/Gameplay/EndingManager.cs
@@ -48,12 +48,14 @@ public class HoardEndingManager : MonoBehaviour
         if (!player1HasMoves)
         {
             Debug.Log("Stalemate detected - one player has no legal moves!");
+            EndGame();
         }
 
         //Check if all pieces are gone
         if (teamCounts[0] <= 0)
         {
-            Debug.Log($"Player 1 has lost all their pieces and thus lost the game!");
+            Debug.Log($"Extinction detected - Team 0 material count: {teamCounts[0]}");
+            Debug.Log($"Actual pieces remaining: {pieceSpawner.players[0].pieces.Count}");
             EndGame();
         }
     }

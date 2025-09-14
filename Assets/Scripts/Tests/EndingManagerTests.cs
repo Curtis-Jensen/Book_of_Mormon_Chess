c7a5f41dc8fdba2b51bbc13593395c8325c0d357
Curtis Jensen <38483758+Curtis-Jensen@users.noreply.github.com>
2025-09-14 09:56:28 -0600

    Added tests folder

---
+++ Assets/Scripts/Tests/EndingManagerTests.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class EndingManagerTests
+{
+    [UnityTest]
+    public IEnumerator CheckStalemate()
+    {
+        yield return null; 
+        // Use the Assert class to test conditions
+    }
+}

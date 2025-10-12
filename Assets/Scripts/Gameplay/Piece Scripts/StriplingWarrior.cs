acd63c29520e3b750b54ff1d0e3d4dc325bb6902
Curtis Jensen <38483758+Curtis-Jensen@users.noreply.github.com>
2025-10-12 09:16:12 -0600

    Now he gets wounded for a few turns!

--- Assets/Scripts/Gameplay/Piece Scripts/StriplingWarrior.cs
+++ Assets/Scripts/Gameplay/Piece Scripts/StriplingWarrior.cs
@@ -6,8 +6,12 @@ using UnityEngine;
 
 public class StriplingWarrior : Piece
 {
+    [SerializeField] int maxWoundedTurns = 2;
+
     //The four cardinal directions, and diagonal moves
-    Vector2Int[] moveDirections =
+    Vector2Int[] moveDirections;
+
+    Vector2Int[] kingMovementPattern =
         { new( 1,  0),
           new(-1,  0),
           new(0,   1),
@@ -16,6 +20,23 @@ public class StriplingWarrior : Piece
           new(-1, -1),
           new(-1,  1),
           new( 1, -1) };
+
+    int currentWoundedTurns = 0;
+
+    protected override void Start()
+    {
+        SetupWounding();
+        base.Start();
+    }
+
+    void SetupWounding()
+    {
+        moveDirections = kingMovementPattern;
+
+        maxWoundedTurns *= 2; //Convert from player turns to full turns
+
+        TurnManager.Instance.OnMoveEnd += DecrementWoundedCounter;
+    }
 
     public override List<Vector2Int> GetMoves()
     {
@@ -40,12 +61,26 @@ public class StriplingWarrior : Piece
 
     public override void Die()
     {
-        if(TurnManager.Instance.selectedPiece.GetType() == typeof(StriplingWarrior))
+        if (TurnManager.Instance.selectedPiece.GetType() == typeof(StriplingWarrior))
         {
             base.Die();
             return;
         }
         TurnManager.Instance.selectedPiece.Die();
         TurnManager.Instance.selectedPiece = this;
+
+        moveDirections = new Vector2Int[0];
+        currentWoundedTurns = maxWoundedTurns;
+    }
+    
+    public void DecrementWoundedCounter()
+    {
+        currentWoundedTurns--;
+        if (currentWoundedTurns <= 0)
+        {
+            moveDirections = kingMovementPattern;
+        }
+
+        Debug.Log($"Stripling Warrior has {currentWoundedTurns} turns left wounded.");
     }
 }

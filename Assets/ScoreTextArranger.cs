1bc8510b2dd22156a66fb3c37c5a0f46f546a7b2
Curtis Jensen <38483758+Curtis-Jensen@users.noreply.github.com>
2026-04-04 16:22:40 -0600

    Highscore text partially put in

--- Assets/ScoreTextArranger.cs
+++ Assets/ScoreTextArranger.cs
@@ -7,17 +7,31 @@ public class ScoreTextArranger : MonoBehaviour
 {
     HoardTurnManager hoardTurnManager;
     TMP_Text scoreText;
+    TMP_Text highScoreText;
+
 
     void Start()
     {
         hoardTurnManager = FindAnyObjectByType<HoardTurnManager>();
         scoreText = GetComponent<TMP_Text>();
 
-        SetNumber();
+        SetScore();
     }
 
-    void SetNumber()
+    void SetScore()
     {
-        scoreText.text = hoardTurnManager.WaveNumber.ToString();
+        int currentScore = hoardTurnManager.WaveNumber;
+        scoreText.text = currentScore.ToString();
+
+        var boardWidth = PlayerPrefs.GetInt("boardSize");
+        string highScoreKey = $"{boardWidth}x{boardWidth}highScore";
+        
+        int highScore = PlayerPrefs.GetInt(highScoreKey, 0);
+        if (currentScore > highScore)
+        {
+            PlayerPrefs.SetInt(highScoreKey, currentScore);
+        }
+
+        highScoreText.text = PlayerPrefs.GetInt(highScoreKey).ToString();
     }
 }

c604458d3474a0b464bff3a6a5a8afe7f1d31539
Curtis Jensen <38483758+Curtis-Jensen@users.noreply.github.com>
2026-04-26 16:32:54 -0600

    Moved the 4 player toggle to a place where it made sense

--- Assets/Scripts/SceneStart/BoardSetup.cs
+++ Assets/Scripts/SceneStart/BoardSetup.cs
@@ -48,6 +48,13 @@ public class BoardSetup : MonoBehaviour
 
     protected virtual void StartPieces()
     {
+        // Resize players list to 2 if 4playerMode is disabled
+        bool fourPlayerMode = PlayerPrefs.GetInt("4playerMode", 1) == 1;
+        if (!fourPlayerMode && pieceSpawner.players.Length > 2)
+        {
+            System.Array.Resize(ref pieceSpawner.players, 2);
+        }
+
         var pieceChoices = RandomizePieces();
         pieceChoices = PlaceKing(pieceChoices);    
         OrderPieces(pieceChoices);
@@ -73,13 +80,6 @@ public class BoardSetup : MonoBehaviour
         TurnManager = GetComponent<TurnManager>();
         TurnManager.boardSize = boardSize;
         pieceSpawner = GetComponent<PieceSpawner>();
-        
-        // Resize players list to 2 if 4playerMode is disabled
-        bool fourPlayerMode = PlayerPrefs.GetInt("4playerMode", 1) == 1;
-        if (!fourPlayerMode && pieceSpawner.players.Length > 2)
-        {
-            System.Array.Resize(ref pieceSpawner.players, 2);
-        }
         
         //If the int comes in as 1 that means true
         aiManager = GetComponent<AiManager>();
